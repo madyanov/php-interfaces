56f5b116925e3229f34ca04aacd9a3b635b932d6
Roman Madyanov <romantaken@gmail.com>
2017-08-18 21:11:20 +0300

    * update

--- src/Storage/StorageInterface.php
+++ src/Storage/StorageInterface.php
@@ -8,5 +8,5 @@ interface StorageInterface extends CacheInterface
 {
     public function increment(string $key, int $offset = 1);
     public function decrement(string $key, int $offset = 1);
-    public function add(string $key, $value, int $seconds = 0);
+    public function add(string $key, $value, int $ttl = 0);
 }
